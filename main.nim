2801a45958a1ea83f0c6516deee323386f7b9644
Coconut0211 <ilyaserov02@gmail.com>
2025-05-08 20:23:11 +0300

    1

--- main.nim
+++ main.nim
@@ -35,36 +35,46 @@ type
     dstIP: string
 
 proc getEthernet(data: var seq[string]): Ethernet =
-  ## Реализуйте обработку Ethernet-уровня
-  ## Можно взять заготовку из лекции
-  # реализуйте код тут
+  result.macDST = data[0..5].join(":")
+  result.macSRC = data[6..11].join(":")
+  case data[12..13].join(""):
+    of "0800": result.protocol = v4
+    of "86dd": result.protocol = v6
+    of "0806": result.protocol = TCP
+    of "0801": result.protocol = UDP
   data = data[14..^1]
 
 proc getIP(data: var seq[string]): IPv4 =
-  ## Реализуйте обработку IP-уровня
-  ## Можно взять заготовку из лекции
-  # реализуйте код тут
+  result.version = ($data[0][0]).parseInt
+  result.headerLength = ($data[0][1]).parseInt  * 4
+  result.service = data[1].parseHexInt #Если использовать parseInt, то не проходит несколько тестов
+  result.totalLength = data[2..3].join("").parseHexInt
+  result.identification = data[4..5].join("")
+  result.flags = data[6..7].join("")
+  result.ttl = data[8].parseHexInt
+  result.protocol = case data[9]:
+    of "06": TCP
+    of "17": UDP
+    else: NONE
+  result.checksum = data[10..11].join("")
+  result.srcIP = data[12..15].mapIt(it.parseHexInt).join(".")
+  result.dstIP = data[16..19].mapIt(it.parseHexInt).join(".")
   data = data[20..^1]
 
 proc getUDP(data: var seq[string]): Protocol =
-  ## Реализуйте обработку протокола UDP
-  ## Можно взять заготовку из лекции
-  ## Учтите, полезной нагрузки может не быть.
-  # реализуйте код тут
+  result.portSRC = data[0..1].join.parseHexInt
+  result.portDST = data[2..3].join.parseHexInt
+  result.length = data[4..5].join.parseHexInt
+  result.checksum = data[6..7].join
+  result.payload = data[8..^1].join.toLower
   data = @[]
 
 proc getTCP(data: var seq[string]): Protocol =
-  ## Реализуйте самостоятельно.
-  ## Согласно RFC:
-  ## 
-  ## - порт источника: первые 2 байта
-  ## - порт назначения: вторые 2 байта
-  ## - размер пакета: половина 13 байта * 4
-  ## - сумма: 17 и 18 байт
-  ## - полезная нагрузка это размер пакета + 1 в одну строку и до конца в нижнем регистре
-  ## 
-  ## Учтите, полезной нагрузки может не быть.
-  # реализуйте код тут
+  result.portSRC = data[0..1].join.parseHexInt
+  result.portDST = data[2..3].join.parseHexInt
+  result.length = ($data[12][0]).parseHexInt * 4
+  result.checksum = data[16..17].join
+  result.payload = data[result.length..^1].join.toLower
   data = @[]
 
 proc test1() =
